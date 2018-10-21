a8abee6fc7375ab5226363aed5fb68193d3c646d
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-10-21 21:09:34 +0200

    Convert to .net framewrok 4.6.1

--- AutoUpdateUtils/Class1.cs
+++ AutoUpdateUtils/Class1.cs
@@ -4,5 +4,19 @@ namespace AutoUpdateUtils
 {
     public class Class1
     {
+        /*
+         * AutoUpdateHost
+            Console.WriteLine("Starting AutoUpdate Host service");
+            var infoChecker = new InfoChecker();
+            infoChecker.GetNewVersionInfo();
+            if (infoChecker.NewVersionAvailable &&
+                MessageBox.Show("Newer version available. Do you want to make an update?", "TODO:bot name",
+                MessageBoxButtons.YesNo) == DialogResult.OK)
+            {
+                infoChecker.
+            }
+            Console.WriteLine("Closing AutoUpdate Host Service");
+            Console.ReadLine();
+            */
     }
 }
