bc793c9a5db2d5797dce257a1632bc636af486b2
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-10-22 19:53:44 +0200

    All scenarious for auto update

--- BotHost/Program.cs
+++ BotHost/Program.cs
@@ -1,4 +1,6 @@
-﻿using BotHost.Properties;
+﻿using AutoUpdateLib;
+using AutoUpdateLib.Interfaces;
+using BotHost.Properties;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,16 +20,89 @@ namespace BotHost
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            IRepoChecker repoChecker = new RepoChecker();
+
             try
             {
-                System.Diagnostics.Process.Start(Settings.Default.BotExePath);
+                DialogResult retry;
+                //Check for updates
+                do
+                {
+                    retry = DialogResult.Cancel;
+                    dynamic requestResult = repoChecker.CheckForUpdates();
+                    if (requestResult.Success)
+                    {
+                        if (requestResult.Result && MessageBox.Show(null,
+                            "Update Bot to newer version?",
+                            "Updates available",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            requestResult = repoChecker.DownloadUpdate();
+                            if (requestResult.Success)
+                            {
+                                requestResult = repoChecker.UpdateApplication(requestResult.Result,
+                                    System.IO.Path.GetDirectoryName(Settings.Default.BotExePath));
+                                if (requestResult.Success)
+                                {
+                                    if (requestResult.Result)
+                                    {
+                                        MessageBox.Show(null,
+                                        "Bot Updated to newer version!",
+                                        null,
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+                                    }
+                                    else
+                                    {
+
+                                        MessageBox.Show(null,
+                                        "Bot was nto updated to newer version!",
+                                        null,
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Exclamation);
+                                    }
+                                }
+                                else
+                                {
+                                    MessageBox.Show(null, requestResult.Exception.Message,
+                                    "Can't update Bot",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Exclamation);
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show(null, requestResult.Exception.Message,
+                                "Can't download Update",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Exclamation);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        retry = MessageBox.Show(null, requestResult.Exception.Message,
+                        "Checking for Updates",
+                        MessageBoxButtons.RetryCancel,
+                        MessageBoxIcon.Exclamation);
+                    }
+                } while (retry == DialogResult.Retry);
             }
-            catch (Exception e)
+            finally
             {
-                MessageBox.Show(null, e.Message, 
-                    "Fatal Application Error!", 
-                    MessageBoxButtons.OK, 
-                    MessageBoxIcon.Exclamation);
+                //Run Bot Application
+                try
+                {
+                    System.Diagnostics.Process.Start(Settings.Default.BotExePath);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(null, e.Message,
+                        "Fatal Application Error!",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+                }
             }
         }
     }
